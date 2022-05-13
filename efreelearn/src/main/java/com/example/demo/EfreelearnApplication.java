fb148f170925cb2244c7f9329c4e26656cd38b94
Andrews <andrewsselvaraj@gmail.com>
2022-05-14 01:26:56 +0530

    OK oK

--- efreelearn/src/main/java/com/example/demo/EfreelearnApplication.java
+++ efreelearn/src/main/java/com/example/demo/EfreelearnApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class EfreelearnApplication {
 
 	public static void main(String[] args) {
+		System.out.println("okok");
 		SpringApplication.run(EfreelearnApplication.class, args);
 	}
 
