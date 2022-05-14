bb480965fcd25f3eab5d55da6a1f2aee376dd269
Andrews <andrewsselvaraj@gmail.com>
2022-05-14 23:28:57 +0530

    update application.java

--- efreelearn/src/main/java/com/example/demo/EfreelearnApplication.java
+++ efreelearn/src/main/java/com/example/demo/EfreelearnApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class EfreelearnApplication {
 
 	public static void main(String[] args) {
-		System.out.println("okok");
+		System.out.println("ok...ok");
 		SpringApplication.run(EfreelearnApplication.class, args);
 	}
 
